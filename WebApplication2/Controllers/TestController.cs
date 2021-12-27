afebd9f179464c1886a22d3f55f9781a67868014
育程 詹 <w983874@gmail.com>
2021-12-27 17:51:16 +0800

    v.1.3 v.1.4

--- WebApplication2/Controllers/TestController.cs
+++ WebApplication2/Controllers/TestController.cs
@@ -9,18 +9,67 @@ namespace WebApplication2.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test
-        public ActionResult Index()
+        static List<Person> data = new List<Person>()
         {
-            var data = new List<Person>()
-            {
                 new Person() { Id = 1, Name = "Will ", Age = 18},
                 new Person() { Id = 2, Name = "Will2 ", Age = 28},
                 new Person() { Id = 3, Name = "Will3 ", Age = 38},
                 new Person() { Id = 4, Name = "Will4 ", Age = 48},
-            };
+        };
+
+        public ActionResult Index()
+        {
 
             return View(data);
         }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                // Todo
+                data.Add(person);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(data.FirstOrDefault(p => p.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                var payload = data.FirstOrDefault(p => p.Id == id);
+                payload.Name = person.Name;
+                payload.Age = person.Age;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var payload = data.FirstOrDefault(p => p.Id == id);
+
+            return View(payload);
+        }
+
     }
 }
