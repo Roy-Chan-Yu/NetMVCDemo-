9f837a124c67a67bbf929d5bd00f090c8a7dff72
育程 詹 <w983874@gmail.com>
2021-12-28 10:38:02 +0800

    add ModelDisplayName

--- WebApplication2/Models/Person.cs
+++ WebApplication2/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,11 @@ namespace WebApplication2.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType = typeof(Resource1))]
+        [DisplayName("姓名")]
         public string Name { get; set; }
         [Required]
         [Range(0, 200)]
+        [DisplayName("年齡")]
         public int Age { get; set; }
     }
 }
