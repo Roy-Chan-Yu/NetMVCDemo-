1c0013dbf8294a2644f1903de517b0b2667447bc
育程 詹 <w983874@gmail.com>
2021-12-28 17:13:51 +0800

    add Empty EF and views mappings

---
+++ WebApplication2/Controllers/ConsigneeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ConsigneeController : Controller
+    {
+        ContosoUniversityEntities2 db = new ContosoUniversityEntities2();
+        // GET: Consignee
+        public ActionResult Index()
+        {
+            return View(db.consignee_manageme.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.user_info_user_id = new SelectList(db.user_info, "user_id", "user_name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(consignee_manageme cons)
+        {
+            if (ModelState.IsValid)
+            {
+                db.consignee_manageme.Add(cons);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_info_user_id = new SelectList(db.user_info, "user_id", "user_name");
+            return View(cons);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return HttpNotFound();
+            }
+
+            var item = db.consignee_manageme.Find(id.Value);
+
+            ViewBag.user_info_user_id = new SelectList(db.user_info, "user_id", "user_name");
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, consignee_manageme cons)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.consignee_manageme.Find(id);
+
+                item.consignee_name = cons.consignee_name;
+                item.consignee_address = cons.consignee_address;
+                item.consignee_code = cons.consignee_code;
+                item.cinsignee_phone = cons.cinsignee_phone;
+                item.user_info_user_id = cons.user_info_user_id;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(db.consignee_manageme.Find(id));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            consignee_manageme cons = db.consignee_manageme.Find(id);
+            if (cons == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(cons);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var item = db.consignee_manageme.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+
+            consignee_manageme payload = db.consignee_manageme.Find(id);
+            db.consignee_manageme.Remove(payload);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication2/Controllers/user_infoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class user_infoController : Controller
+    {
+        private ContosoUniversityEntities2 db = new ContosoUniversityEntities2();
+
+        // GET: user_info
+        public ActionResult Index()
+        {
+            return View(db.user_info.ToList());
+        }
+
+        // GET: user_info/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_info user_info = db.user_info.Find(id);
+            if (user_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_info);
+        }
+
+        // GET: user_info/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: user_info/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,user_name,user_sex,user_phone,user_pw,user_state")] user_info user_info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user_info.Add(user_info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user_info);
+        }
+
+        // GET: user_info/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_info user_info = db.user_info.Find(id);
+            if (user_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_info);
+        }
+
+        // POST: user_info/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "user_id,user_name,user_sex,user_phone,user_pw,user_state")] user_info user_info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_info);
+        }
+
+        // GET: user_info/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_info user_info = db.user_info.Find(id);
+            if (user_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_info);
+        }
+
+        // POST: user_info/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user_info user_info = db.user_info.Find(id);
+            db.user_info.Remove(user_info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
