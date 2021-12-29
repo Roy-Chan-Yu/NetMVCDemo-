76d757d9045a852404e9986e9a1f7c59d587a3ef
育程 詹 <w983874@gmail.com>
2021-12-29 08:54:42 +0800

    modify the DB-Model First and upd the Class-Model sync

--- WebApplication2/Models/consignee_manageme.cs
+++ WebApplication2/Models/consignee_manageme.cs
@@ -11,8 +11,7 @@ namespace WebApplication2.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class consignee_manageme
     {
         public int Id { get; set; }
@@ -21,7 +20,8 @@ namespace WebApplication2.Models
         public string consignee_code { get; set; }
         public string cinsignee_phone { get; set; }
         public int user_info_user_id { get; set; }
-
+        public string Memo { get; set; }
+    
         public virtual user_info user_info { get; set; }
     }
 }
