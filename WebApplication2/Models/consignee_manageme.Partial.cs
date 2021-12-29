e3c91e19b86737434b3ee609efe06940e1777922
育程 詹 <w983874@gmail.com>
2021-12-29 09:48:08 +0800

    add consignee_manageme model body class -> (partial class)

---
+++ WebApplication2/Models/consignee_manageme.Partial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    [MetadataType(typeof(consignee_managemeMetadata))]
+    public partial class consignee_manageme
+    {
+
+        public class consignee_managemeMetadata
+        {
+            public int Id { get; set; }
+            [Required]
+            public string consignee_name { get; set; }
+            public string consignee_address { get; set; }
+            public string consignee_code { get; set; }
+            public string cinsignee_phone { get; set; }
+            public int user_info_user_id { get; set; }
+            public string Memo { get; set; }
+
+            public virtual user_info user_info { get; set; }
+
+        }
+    }
+}
